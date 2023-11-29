56fe474e2b60cf5778ff4e85de47cf3e6a3adae3
Jhon Gómez Marín <gmezjhon@gmail.com>
2023-11-29 17:37:28 -0500

    Config main components

---
+++ src/app/pages/auth/login-admin/login-admin.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login-admin',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './login-admin.component.html',
+  styleUrls: ['./login-admin.component.scss']
+})
+export class LoginAdminComponent {
+
+}
