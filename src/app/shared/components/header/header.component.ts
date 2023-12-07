14d5fc3ad1da069920dc7cea5cbcc2138cb71b2e
Jhon Gómez Marín <gmezjhon@gmail.com>
2023-12-07 12:06:03 -0500

    Modified prefix

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-header',
+  selector: 'sc-header',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './header.component.html',
